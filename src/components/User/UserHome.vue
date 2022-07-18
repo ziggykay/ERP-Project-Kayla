0cd78b9ac3e6b45bc1904d6f0382ecba0e0b4628
CodeSH85 <codesh85@gmail.com>
2022-07-18 14:04:59 +0800

    temp

--- src/components/User/UserHome.vue
+++ src/components/User/UserHome.vue
@@ -1,19 +1,30 @@
 <template>
-  <div class="container-fluid d-flex">
+  <div class="container-fluid">
+    <div class="row">
 
-    <!-- 出勤 -->
-    <div class="content-box punch-box col-5 d-flex">
-      <p class="title"><strong>今日打卡狀態</strong></p>
-      <hr/>
-      <p>fdfs</p>
-    </div>
+      <!-- 出勤 -->
+      <div class="content-box punch-box col">
+        <p class="title"><strong>今日打卡狀態</strong></p>
+        <hr/>
+        <div class="container d-flex justify-content-around">
+          <p>日期</p>
+          <p>姓名</p>
+          <p>簽到</p>
+          <p>簽退</p>
+        </div>
+        <div class="container d-flex justify-content-around">
+          <p>{{punchData[0].date}}</p>
+          <p>王小明</p>
+          <p>08:50</p>
+          <p>16:37</p>
+        </div>
+      </div>
 
-    <!-- 課程 -->
-    <div class="content-box class-box col-5 d-flex">
-      <p class="title"><strong>課程</strong></p>
-      <hr/>
-      <div class="scroll-box" >
-        <div class="class-card content-box">
+      <!-- 課程 -->
+      <div class="content-box class-box col">
+        <p class="title"><strong>今日課程</strong></p>
+        <hr/>
+        <div class="class-card">
           <div class="class-title d-flex justify-content-between">
             <p class="title">{{ todayClass.name }}</p>
             <button type="button" class="btn confirm-btn watch-btn" @click="updateVideo"
@@ -37,28 +48,49 @@
             </div>
           </div>
         </div>
-        <div class="error" v-if="todayData == ''">沒有課程可以顯示</div>
+        <div class="error" v-if="todayClass == ''">沒有課程可以顯示</div>
+      </div>
+
+      <!-- 日誌 -->
+      <div class="content-box diary-box col-2">
+        <p class="title"><strong>日誌登打狀態</strong></p>
+        <hr/>
+        <div class="container">
+          <p>{{ diaryMsg }}</p>
+          <div :class="[ diaryData == 0 ? 'diarySign-red' : 'diarySign-green' ]"></div>
+        </div>
       </div>
     </div>
-
-    <!-- 日誌 -->
-    <div class="content-box col-2 diary-box">
-      <div class="">test</div>
-    </div>
-
   </div>
-
+  
+  <!-- 職缺 -->
   <div class="content-box job-box">
     <p class="title"><strong>推薦職缺</strong></p>
     <hr>
+    <div class="container">
+      <div class="content-box-border job-card">
+        <p></p>
+        <hr/>
+        <p></p>
+      </div>
+    </div>
   </div>
 
 </template>
 
 <script setup>
-
 import {ref} from "vue"
 
+// punch
+const punchData = ref([
+  {
+    date: '20220718',
+    name: '王小明',
+    in: '08:55',
+    out: '16:37',
+  }
+])
+// class
 const todayClass = ref({
   id: 2,
   name: 'Jquery&Jquery extensions',
@@ -68,6 +100,24 @@ const todayClass = ref({
   done: 40,
 })
 
+// diary
+const diaryMsg = ref('尚未登打日誌')
+const diaryData = ref(1)
+
+// job
+const jobData = ref([
+  {
+    title: '前端工程師',
+    skillSet:[
+      'JavaScript', 'BootStrap', 'Node.JS'
+    ],
+    location: '台北',
+    platform: '104'
+  },
+
+])
+
+
 </script>
 
 <style lang="scss" scoped>
@@ -75,20 +125,72 @@ const todayClass = ref({
 .title{
   width: auto;
 }
-
 .punch-box{
-  hr{
-    color: red;
+
+}
+.class-box{   
+  flex-direction: column;
+  width:  auto;
+  height: auto;
+  max-height: 100%;
+  .class-card{
+    width: auto;
+    height: auto;
+    .title{
+      color: rgb(52, 52, 52);
+      font-size: 1.2rem;
+      margin-top: 1rem;
+    }
+    .watch-btn{
+      height: 2.2rem;
+      margin-top: 1rem;
+    }
+    .class-content{
+      .percent-section{
+        text-align: center;
+        padding-top: 1rem; 
+        .percent-number{
+          font-size: 1.8rem;
+          margin-bottom: 0%;
+        }
+        .percent-desc{
+          font-size: 0.8rem;
+        }
+      }
+      .bar-section{
+        padding-top: 1.5rem;
+        .bar-label{
+          display: inline;
+          margin: 1rem 1rem 0 0;
+          font-size: 0.8rem;
+        }
+        .progress{
+          margin-top: 1rem;
+          border-style: solid;
+          border-width: 1px;
+          border-color: #74abdd;
+          background-color: transparent;
+          .progress-bar{
+            background-color: rgb(0, 200, 236);
+          }
+        }
+      }
+    }
   }
 }
-
-.class-box{   
-  flex-direction: column;  
-  width: auto;
+.diarySign-red{
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: rgb(224, 12, 12);
+}
+.diarySign-green{
+  @extend .diarySign-red;
+  background-color: rgb(12, 224, 40);
+}
+.job-box{
+  width:  auto;
   height: auto;
-  max-height: 100vh;
-  hr{
-    margin: 0;
-  }
+  max-height: 100%;
 }
 </style>
