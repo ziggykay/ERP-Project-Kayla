450aa07ce3502a827c42224921c05c924e1d03f9
jessie0219 <nini880219@gmail.com>
2022-07-22 21:07:51 +0800

    修改67%的跑版 : 用戶端登打日誌頁面

--- src/components/User/CreateDiaryPage.vue
+++ src/components/User/CreateDiaryPage.vue
@@ -1,23 +1,21 @@
 <template>
   <div class="section">
-    <div class="container-fluid">
-      <div class="d-md-flex">
-        <!-- 個人資訊 -->
-        <div class="content-box userInfo-box col d-flex">
-          <div v-for="data in info" class="container-fluid p-2">
-            <p class="text-primary InfoTitle"><strong>個人資訊</strong></p>
-            <hr />
-            <p class="Info"><strong>Class : </strong>{{ data.Class }}</p>
-            <hr />
-            <p class="Info"><strong>Number : </strong>{{ data.Id }}</p>
-            <hr />
-            <p class="Info"><strong>Name : </strong>{{ data.Name }}</p>
-            <hr />
-            <p class="Info"><strong>Email : </strong>{{ data.Email }}</p>
-          </div>
+    <div class="d-md-flex">
+      <div class="content-box diary-box col">
+        <div v-for="data in info" class="container p-2">
+          <p class="text-primary InfoTitle"><strong>個人資訊</strong></p>
+          <hr />
+          <p class="Info"><strong>Class : </strong>{{ data.Class }}</p>
+          <hr />
+          <p class="Info"><strong>Number : </strong>{{ data.Id }}</p>
+          <hr />
+          <p class="Info"><strong>Name : </strong>{{ data.Name }}</p>
+          <hr />
+          <p class="Info"><strong>Email : </strong>{{ data.Email }}</p>
         </div>
-        <!-- 已登打日誌 -->
-        <div class="content-box diary-box col">
+      </div>
+      <div class="container-fluid d-md-flex">
+        <div class="content-box userInfo-box col d-flex">
           <div class="container-fluid p-2">
             <p class="text-primary InfoTitle"><strong>已編輯日誌</strong></p>
             <hr />
@@ -47,77 +45,76 @@
           </div>
         </div>
       </div>
-      <!-- 登打日誌 -->
-      <div class="content-box create-box col">
-        <div class="container-fluid p-2">
-          <p class="text-primary InfoTitle"><strong>日誌登打</strong></p>
-          <hr />
-          <div class="d-flex flex-wrap">
-            <div class="work-time col-4">
-              <label for=""
-                >工作時數 <span class="note">以0.5h為單位</span></label
-              >
-              <br />
-              <select id="time">
-                <option value="">請選擇時數</option>
-                <option value="0.5">0.5</option>
-                <option value="1">1</option>
-                <option value="1.5">1.5</option>
-                <option value="2">2</option>
-                <option value="2.5">2.5</option>
-                <option value="3">3</option>
-                <option value="3.5">3.5</option>
-                <option value="4">4</option>
-                <option value="4.5">4.5</option>
-                <option value="5">5</option>
-                <option value="5.5">5.5</option>
-                <option value="6">6</option>
-                <option value="6.5">6.5</option>
-                <option value="7">7</option>
-                <option value="7.5">7.5</option>
-                <option value="8">8</option>
-              </select>
-            </div>
-            <div class="work-time col-4">
-              <label for="project">專案選擇</label>
-              <br />
-              <select id="project">
-                <option value="">請選擇專案類型</option>
-                <option value="專案">專案</option>
-                <option value="產品">產品</option>
-              </select>
-            </div>
-            <div class="work-time col-4">
-              <label for="profile_pic">上傳圖片</label>
-              <br />
-              <input
-                type="file"
-                id="profile_pic"
-                name="profile_pic"
-                accept=".jpg, .jpeg, .png"
-              />
-            </div>
+    </div>
+    <div class="content-box create-box col">
+      <div class="container-fluid p-2 col-12">
+        <p class="text-primary InfoTitle"><strong>日誌登打</strong></p>
+        <hr />
+        <div class="d-flex flex-wrap">
+          <div class="work-time col-md-4">
+            <label for=""
+              >工作時數 <span class="note">以0.5h為單位</span></label
+            >
             <br />
-            <div class="container-fluid work-time col-12">
-              <label for="">內文</label>
-              <br />
-              <textarea
-                v-model.trim="textarea"
-                class="textarea"
-                placeholder="請輸入內文..."
-                id="textarea"
-              ></textarea>
-            </div>
+            <select id="time">
+              <option value="">請選擇時數</option>
+              <option value="0.5">0.5</option>
+              <option value="1">1</option>
+              <option value="1.5">1.5</option>
+              <option value="2">2</option>
+              <option value="2.5">2.5</option>
+              <option value="3">3</option>
+              <option value="3.5">3.5</option>
+              <option value="4">4</option>
+              <option value="4.5">4.5</option>
+              <option value="5">5</option>
+              <option value="5.5">5.5</option>
+              <option value="6">6</option>
+              <option value="6.5">6.5</option>
+              <option value="7">7</option>
+              <option value="7.5">7.5</option>
+              <option value="8">8</option>
+            </select>
           </div>
-          <div class="text-end">
+          <div class="work-time col-md-4">
+            <label for="project">專案選擇</label>
+            <br />
+            <select id="project">
+              <option value="">請選擇專案類型</option>
+              <option value="專案">專案</option>
+              <option value="產品">產品</option>
+            </select>
+          </div>
+          <div class="work-time col-md-4">
+            <label for="profile_pic">上傳圖片</label>
+            <br />
             <input
-              id="submit"
-              type="submit"
-              class="btn btn-primary add-items confirm-btn"
-              @click="addProject"
-              value="+ 新增專案"
+              type="file"
+              id="profile_pic"
+              name="profile_pic"
+              accept=".jpg, .jpeg, .png"
             />
           </div>
+          <br />
+        </div>
+        <div class="container-fluid work-time col-12">
+          <label for="">內文</label>
+          <br />
+          <textarea
+            v-model.trim="textarea"
+            class="textarea"
+            placeholder="請輸入內文..."
+            id="textarea"
+          ></textarea>
+        </div>
+        <div class="text-end">
+          <input
+            id="submit"
+            type="submit"
+            class="btn btn-primary add-items confirm-btn"
+            @click="addProject"
+            value="+ 新增專案"
+          />
         </div>
       </div>
     </div>
@@ -125,7 +122,10 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
+import overallVue from "../baseComponents/Overall.vue";
+import ClassCard from "../../components/baseComponents/ClassCard.vue";
+import axios from "axios";
+import { ref, onMounted, reactive, watch, defineAsyncComponent } from "vue";
 
 const info = ref([
   {
@@ -146,26 +146,13 @@ const diary = ref([
     project: "C產品",
   },
 ]);
-
-// const textarea = ref();
-// const img = ref();
-// const items = ref([]);
-function addProject() {
-  var selectTime = document.getElementById("time").value;
-  var selectProject = document.getElementById("project").value;
-  var textarea = document.getElementById("textarea").value;
-  var image = document.getElementById("profile_pic").value;
-  var diary = { selectTime, selectProject, image, textarea };
-  localStorage.setItem("diary", JSON.stringify(diary));
-  console.log(diary);
-  alert("新增成功!");
-}
 </script>
 
 <style lang="scss" scoped>
-body {
-  margin: 0;
-  padding: 0;
+.content-box {
+  height: auto;
+  flex-direction: column;
+  padding-right: 20px;
 }
 .create-box {
   width: auto;
@@ -173,7 +160,7 @@ body {
   flex-direction: column;
   padding-right: 20px;
   select {
-    width: 80%;
+    width: 100%;
   }
   .textarea {
     width: 100%;
@@ -181,7 +168,7 @@ body {
     border-radius: 5px;
     .Info {
       p {
-        font-size: 0.8rem;
+        font-size: 1rem;
       }
     }
     .project {
@@ -196,23 +183,22 @@ body {
   }
 }
 
-.userInfo-box {
-  flex-direction: column;
-  width: auto;
-  height: auto;
-  max-height: 100vh;
-  hr {
-    margin: 0;
-  }
-  p.Info {
-    margin-top: 1rem;
-    margin-bottom: 1rem;
-  }
-}
+// .userInfo-box {
+//   flex-direction: column;
+//   width: auto;
+//   height: auto;
+//   max-height: 100vh;
+//   hr {
+//     margin: 0;
+//   }
+//   p.Info {
+//     margin-top: 1rem;
+//     margin-bottom: 1rem;
+//   }
+// }
 .diary-box {
   display: flex;
   flex-direction: column;
-  width: auto;
   height: auto;
   max-height: 100vh;
 }
