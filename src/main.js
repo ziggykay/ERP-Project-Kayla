da58c5e623906c5f89e52eea700ec1d60772b572
CodeSH85 <codesh85@gmail.com>
2022-07-25 13:53:37 +0800

    fix store in main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { Store } from 'vuex'
 // import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 import "./styles/custom.scss"
@@ -35,6 +36,7 @@ import 'xe-utils'
 import VXETable from 'vxe-table'
 import 'vxe-table/lib/style.css'
 
+const store = new Store()
 
 
 createApp(App).use(router).use(store).component('Datepicker', Datepicker).component('v-chart', echarts).use(VXETable)
