2224cfac7ce1ba387315ca2c42bfac9185ff9571
jessie0219 <nini880219@gmail.com>
2022-07-31 17:37:08 +0800

    login頁面

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,74 +1,180 @@
 <template>
-  <div class="container text-center">
-    <div class="">
-      <label for="">選擇身分</label>
+  <div class="container text-center inner">
+    <div class="py-3"><h2 class="text-primary">Logo</h2></div>
+    <div class="inputs">
+      <div class="d-flex justify-content-center inputWidth">
+        <div class="align-self-top mx-1 access">
+          <div class="text-start">
+            <label for="name" class="text-primary">選擇登入身份</label>
+          </div>
+          <div>
+            <select>
+              <option>選擇身份</option>
+
+              <option>學生</option>
+              <option>管理員</option>
+              <option>企業</option>
+            </select>
+          </div>
+        </div>
+        <div class="align-self-top mx-1 access">
+          <div class="text-start">
+            <label for="name" class="text-primary">選擇班級</label>
+          </div>
+          <div>
+            <select>
+              <option>選擇班級</option>
+              <option>dv102</option>
+              <option>fn102</option>
+              <option>se211</option>
+            </select>
+          </div>
+        </div>
+      </div>
+      <div class="d-flex justify-content-center inputWidth">
+        <div class="align-self-top inputPassword">
+          <span>
+            <div class="mt-4 text-start">
+              <label for="name" class="text-primary">姓名</label>
+            </div>
+            <div>
+              <input id="name" type="name" />
+            </div>
+          </span>
+        </div>
+      </div>
+      <div class="d-flex justify-content-center inputWidth">
+        <div class="align-self-top inputPassword">
+          <span>
+            <div class="mt-4 text-start">
+              <label for="name" class="text-primary">密碼</label>
+            </div>
+            <div>
+              <input id="name" type="name" />
+            </div>
+          </span>
+        </div>
+      </div>
+      <!-- <div class="d-flex justify-content-center inputWidth">
+        <div class="align-self-top inputPassword">
+          <span>
+            <div class="mt-4 text-start">
+              <label for="name" class="text-primary">驗證碼</label>
+            </div>
+            <div>
+              <input id="name" type="name" />
+            </div>
+          </span>
+        </div>
+      </div> -->
+      <!-- <div class="d-flex justify-content-center mt-4 inputWidth">
+        <div class="align-self-top">
+          <span class="mt-4">
+            <input id="name" value="AkB48" />
+          </span>
+          <span class="mt-4">
+            <i class="fa-solid fa-rotate-left text-secondary mx-1"></i>
+          </span>
+          <span class="mt-4">
+            <i class="fa-solid fa-volume-high text-secondary mx-1"></i>
+          </span>
+        </div>
+      </div> -->
+    </div>
+    <!-- <span class="mt-4">
+      <label class="mt-4 text-primary" for="">選擇登入身份</label>
+      <br />
       <select>
-          <option>學生</option>
-          <option>管理員</option>
-          <option>企業</option>
+        <option>學生</option>
+        <option>管理員</option>
+        <option>企業</option>
       </select>
-    </div>
-    <br/>
-    <div class="">
+    </span>
+
+    <span class="mt-4">
       <label for="name">姓名</label>
-      <input id="name" type="name"/>
-    </div>
-    <br/>
-    <div class="">
+      <br />
+      <input id="name" type="name" />
+    </span>
+    <br />
+    <div class="mt-4">
       <label for="passWord">密碼</label>
-      <input id="passWord" type="password"/>
+      <input id="passWord" type="password" />
     </div>
-    <div class="">
+    <div class="mt-5">
       <label for="passWord">驗證碼</label>
-      <input id="passWord" type="password"/>
+      <input id="passWord" type="password" />
     </div>
-    <br/>
+    <br /> -->
     <!-- <router-link to="/home">
       <button class="btn btn-primary text-light">Login</button>
     </router-link> -->
-    <button class="btn btn-primary text-light" @click="toHomePage">Login</button>
+    <button class="btn btn-primary text-light my-4" @click="toHomePage">
+      Login
+    </button>
   </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
+import { useRouter, useRoute } from "vue-router";
 
-import { ref } from "vue"
-import { useRouter, useRoute } from "vue-router"
+const router = useRouter();
+const route = useRoute();
 
-const router = useRouter()
-const route = useRoute()
-
-const loginAuth = ref(
-  {
-    access: 1
-  }
-)
+const loginAuth = ref({
+  access: 1,
+});
 
 const toHomePage = () => {
   // router.push('/user/home')
   switch (loginAuth.value.access) {
     case 1:
-      router.push('/user/home')
+      router.push("/user/home");
       break;
     case 2:
-      router.push('/manager/home')
+      router.push("/manager/home");
       break;
     case 3:
-      router.push('/company/home')
+      router.push("/company/home");
       break;
     default:
-      router.push('/')
+      router.push("/");
       break;
   }
-}
-
+};
 </script>
 
 <style lang="scss" scoped>
-.container{
-  width: auto;
+// body {
+//   background-color: #558aba;
+// }
+.inner {
+  margin-top: 2.5rem;
+  width: 60%;
+  // width: auto;
   height: auto;
+  // height: 80vh;
   background: rgb(255, 255, 255);
-  
+  border-radius: 10px;
+  .inputs {
+    width: auto;
+    label {
+      font-size: 0.6rem;
+    }
+    .inputWidth {
+      width: 100%;
+      .access {
+        select {
+          width: 100%;
+        }
+      }
+      .inputPassword {
+        input {
+          width: 100%;
+        }
+      }
+    }
+  }
 }
 </style>
