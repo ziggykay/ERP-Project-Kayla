ee98009e544266ba451a72d4c3073acd99f0560d
jessie0219 <nini880219@gmail.com>
2022-08-01 01:10:39 +0800

    更新 : loginPage

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,39 +1,90 @@
 <template>
-  <div class="container text-center">
-    <!-- filter -->
-    <!-- <label for="">身分</label>
-	  	<select class="selectInfo me-2" v-model="type" @change="axiosNumber">
-	  		<option value="">請選擇身份</option>
-				<option v-for="data of selectType" :value="data.item">
-					{{data.name}}
-				</option>
-        <optgroup label="管理、企業">
-          <option value="manager">管理員</option>
-          <option value="ent">企業人員</option>
-        </optgroup>
-	  	</select>
-	  	<select class="selectInfo me-2" v-model="number">
-	  		<option value="">請選擇班級</option>
-				<option v-for="data of selectNumber" :value="data.item">
-					{{ data.name }}
-				</option>	      	 	
-	  	</select> -->
-    <br />
-    <div class="">
+  <div class="container text-center inner">
+    <div class="py-3"><h2 class="text-primary">Logo</h2></div>
+    <div class="inputs">
+      <div class="d-flex justify-content-center inputWidth">
+        <div class="align-self-top mx-1 access">
+          <div class="text-start">
+            <label for="name" class="text-primary">選擇登入身份</label>
+          </div>
+          <div>
+            <select>
+              <option>選擇身份</option>
+              <option>學生</option>
+              <option>管理員</option>
+              <option>企業</option>
+            </select>
+          </div>
+        </div>
+        <div class="align-self-top mx-1 access">
+          <div class="text-start">
+            <label for="name" class="text-primary">選擇班級</label>
+          </div>
+          <div>
+            <select>
+              <option>選擇班級</option>
+              <option>dv102</option>
+              <option>fn102</option>
+              <option>se211</option>
+            </select>
+          </div>
+        </div>
+      </div>
+      <div class="d-flex justify-content-center inputWidth">
+        <div class="align-self-top inputPassword">
+          <span>
+            <div class="mt-4 text-start">
+              <label for="name">姓名</label>
+            </div>
+            <div>
+              <input v-model="name" type="name" />
+            </div>
+          </span>
+        </div>
+      </div>
+      <div class="d-flex justify-content-center inputWidth">
+        <div class="align-self-top inputPassword">
+          <span>
+            <div class="mt-4 text-start">
+              <label for="passWord">密碼</label>
+            </div>
+            <div>
+              <input v-model="password" type="password" />
+            </div>
+          </span>
+        </div>
+      </div>
+    </div>
+    <!-- <span class="mt-4">
+      <label class="mt-4 text-primary" for="">選擇登入身份</label>
+      <br />
+      <select>
+        <option>學生</option>
+        <option>管理員</option>
+        <option>企業</option>
+      </select>
+    </span>
+    <span class="mt-4">
       <label for="name">姓名</label>
-      <input v-model="name" type="name" />
-    </div>
+      <br />
+      <input id="name" type="name" />
+    </span>
     <br />
-    <div class="">
+    <div class="mt-4">
       <label for="passWord">密碼</label>
-      <input v-model="password" type="password" />
+      <input id="passWord" type="password" />
     </div>
-    <br />
-    <div class="err">{{ errMsg }}</div>
-    <button class="btn btn-primary text-light" @click="login">登入</button>
-    <div class="">{{ name }}</div>
-    <div class="">{{ password }}</div>
-    <div class="">{{ nameErr }}</div>
+    <div class="mt-5">
+      <label for="passWord">驗證碼</label>
+      <input id="passWord" type="password" />
+    </div>
+    <br /> -->
+    <!-- <router-link to="/home">
+      <button class="btn btn-primary text-light">Login</button>
+    </router-link> -->
+    <button class="btn btn-primary text-light my-4" @click="login">
+      Login
+    </button>
   </div>
 </template>
 
@@ -175,12 +226,35 @@ const login = async () => {
 </script>
 
 <style lang="scss" scoped>
-.container {
-  width: auto;
+// body {
+//   background-color: #558aba;
+// }
+.inner {
+  margin-top: 2.5rem;
+  width: 60%;
+  // width: auto;
   height: auto;
+  // height: 80vh;
   background: rgb(255, 255, 255);
-}
-.err {
-  color: red;
+  border-radius: 10px;
+  .inputs {
+    width: auto;
+    label {
+      font-size: 0.6rem;
+    }
+    .inputWidth {
+      width: 100%;
+      .access {
+        select {
+          width: 100%;
+        }
+      }
+      .inputPassword {
+        input {
+          width: 100%;
+        }
+      }
+    }
+  }
 }
 </style>
