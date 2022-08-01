07ad0c326b2e95f4fa6c13cc3f59b3e974b97391
jessie0219 <nini880219@gmail.com>
2022-08-01 23:21:12 +0800

    更新 : UserHome CSS

--- src/components/User/UserHome.vue
+++ src/components/User/UserHome.vue
@@ -72,13 +72,16 @@
     <div class="content-box diary-box col-1">
       <p class="title"><strong>日誌登打狀態</strong></p>
       <hr />
-      <div class="container">
-        <p>{{ diaryMsg }}</p>
+      <div class="d-flex container my-2 justify-content-center mt-1">
         <div
+          class="mx-2 align-self-center"
           :class="[
             diaryMsg == '今日尚未登打' ? 'diarySign-red' : 'diarySign-green',
           ]"
         ></div>
+        <div class="mx-2 align-self-center">
+          <p class="diaryMsg mt-3">{{ diaryMsg }}</p>
+        </div>
       </div>
     </div>
   </div>
@@ -93,10 +96,10 @@
         v-for="data of jobData"
         :key="data"
       >
-        <p>{{ data.Job }}</p>
+        <p class="multiline-ellipsis">{{ data.Job }}</p>
         <hr />
         <p>工作地點：{{ data.Region }}</p>
-        <p>技能：{{ data.Skill }}</p>
+        <p class="multiline-ellipsis">技能：{{ data.Skill }}</p>
         <a :href="data.Url">{{ data.Resource }}</a>
       </div>
     </div>
@@ -143,7 +146,7 @@
 import { ref, onMounted } from "vue";
 import store from "../../store";
 import axios from "axios";
-
+const token = store.getters["auth/getToken"];
 // punch
 const punchData = ref([]);
 
@@ -197,7 +200,7 @@ const getTodayDiary = async () => {
   try {
     let href = `http://54.186.56.114/diary/status`;
     let { data } = await axios.get(href, {
-      headers: { authorization: `Bearer ${store.state.token}` },
+      headers: { authorization: `Bearer ${token}` },
     });
     diaryMsg.value = data.data.message;
   } catch (e) {
@@ -213,7 +216,7 @@ const getJob = async () => {
   let href = `http://54.186.56.114/diary/RecommandCareer`;
   try {
     let { data } = await axios.get(href, {
-      headers: { authorization: `Bearer ${store.state.token}` },
+      headers: { authorization: `Bearer ${token}` },
     });
     jobData.value = data.data;
   } catch (e) {
@@ -238,7 +241,7 @@ const sendQuestion = async () => {
   };
   try {
     let { data } = await axios.post(href, postData, {
-      headers: { authorization: `Bearer ${store.state.token}` },
+      headers: { authorization: `Bearer ${token}` },
     });
     // console.log(data.data.status)
     alert("已更新資料");
@@ -413,5 +416,11 @@ const sendQuestion = async () => {
   position: fixed;
   right: 5%;
   bottom: 5%;
+}
+.multiline-ellipsis {
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 5;
+  overflow: hidden;
 }
 </style>
